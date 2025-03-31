b5f033ef2dd2c29dd9ae0ff8a73612ac51b68a3c
m-zalo <m.zaloziecki@gmail.com>
2025-03-31 09:53:32 +0300

    link text change

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Jenkins on Udemy
+          Example Link to example.com
         </a>
       </header>
       <p>
